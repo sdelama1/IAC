3d619a0f3aadeddf929aff055e39a1331f0f7b1c
sdelama1 <sdelama12@gmail.com>
2026-02-06 13:20:35 -0500

    feat: tablas de enrutamiento para segmentacion de trafico

---
+++ route_tables.tf
@@ -0,0 +1,63 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "IAC-Public-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_app" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "IAC-Private-App-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "private_app_1" {
+  subnet_id      = aws_subnet.private_app_1.id
+  route_table_id = aws_route_table.private_app.id
+}
+
+resource "aws_route_table_association" "private_app_2" {
+  subnet_id      = aws_subnet.private_app_2.id
+  route_table_id = aws_route_table.private_app.id
+}
+
+resource "aws_route_table" "private_data" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "IAC-Private-Data-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "private_data_1" {
+  subnet_id      = aws_subnet.private_data_1.id
+  route_table_id = aws_route_table.private_data.id
+}
+
+resource "aws_route_table_association" "private_data_2" {
+  subnet_id      = aws_subnet.private_data_2.id
+  route_table_id = aws_route_table.private_data.id
+}
